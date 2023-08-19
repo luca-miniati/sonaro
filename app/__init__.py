dbc7b35d71df68cb5801cf42af2e3ce862cae6e9
loo <luca.miniati@gmail.com>
2023-08-18 21:05:57 -0700

    update redirect uri

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ client_secret = os.getenv("CLIENT_SECRET")
 sp_oauth = SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
-    redirect_uri='spotifyplayground.onrender.com/redirect',
+    redirect_uri='spotifyplayground.onrender.com/callback',
     scope=['user-library-read', 'user-read-playback-state',
            'user-read-recently-played', 'user-top-read'],
 )
