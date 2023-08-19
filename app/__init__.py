cd6e334a6c1b26a6289c254022abfe434c3a844d
loo <luca.miniati@gmail.com>
2023-08-18 19:41:01 -0700

    update callback uri

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ client_secret = os.getenv("CLIENT_SECRET")
 sp_oauth = SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
-    redirect_uri='http://127.0.0.1:5000/callback',
+    redirect_uri=url_for('callback', _external=True),
     scope=['user-library-read', 'user-read-playback-state',
            'user-read-recently-played', 'user-top-read'],
 )
